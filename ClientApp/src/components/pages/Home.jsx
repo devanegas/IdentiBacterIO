fe229cfa4d7b2e1d46e42de4ab88020214766798
Diego Vanegas <dieguitovan@gmail.com>
2021-04-07 20:34:16 -0600

    Added material ui to packages

--- ClientApp/src/components/pages/Home.jsx
+++ ClientApp/src/components/pages/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Card, TextField } from "@material-ui/core";
 
-const Home = props => {
-    return(
-        <div>
-            Hello World
-        </div>
-    )
-}
+const Home = (props) => {
+	return (
+		<div>
+			<Card elevation={3}>
+				<TextField variant="outlined" margin='dense'/>
+                <TextField variant="outlined" margin='dense'/>
+                <TextField variant="outlined" margin='dense'/>
+                <TextField variant="outlined" margin='dense'/>
+			</Card>
+		</div>
+	);
+};
 
 export default Home;
