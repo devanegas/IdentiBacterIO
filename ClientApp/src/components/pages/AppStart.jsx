d7aab8cc56147003bced4b0a90d5fbb5e0eb347e
Diego Vanegas <dieguitovan@gmail.com>
2021-04-14 20:52:41 -0600

    Changed css of first form

--- ClientApp/src/components/pages/AppStart.jsx
+++ ClientApp/src/components/pages/AppStart.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import * as States from "../../store/states";
 import { useSelector, useDispatch } from "react-redux";
 import { getAppState } from "../../store/reducers/reducer";
-import FormPage from './FormPage'
+import FormPage from "./FormPage";
 
 const AppStart = (props) => {
 	const appState = useSelector(getAppState);
 	return (
-		<div>{appState === States.FORM_STATE ? <FormPage /> : <div>nope</div>}</div>
+		<div style={{ height: "100%" }}>
+			{appState === States.FORM_STATE ? <FormPage /> : <div>nope</div>}
+		</div>
 	);
 };
 
